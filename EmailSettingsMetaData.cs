ba9f511d430597747c5d14ef012d13efc06c53dc
pranjalvatsa <pranjalvatsa86@gmail.com>
2020-09-16 12:15:15 +0530

    Create EmailSettingsMetaData.cs

---
+++ EmailSettingsMetaData.cs
@@ -0,0 +1,7 @@
+   public class EmailSettingsMetaData
+    {
+        public string SmtpServer { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
