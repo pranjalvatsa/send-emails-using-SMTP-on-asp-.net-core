2698eba37cd2f41e1c174eb266eb8110dddcd080
pranjalvatsa <pranjalvatsa86@gmail.com>
2020-09-16 12:25:06 +0530

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,10 @@
+//utility class to store email ids and messages 
+ public class Constants
+ {
+        public static string TO_EMAIL_ID_TEST = <To Email Id>;
+        public static string FROM_EMAIL_ID_TEST = <From Email Id>;
+
+        public static string SUCCESS_MESSAGE = "Your email has been sent successfully. Someone from our sales team will get in touch with you soon.";
+        public static string ERROR_MESSAGE = "Sorry! Your email could not be sent at this time. Please try after a while.";
+
+ }
